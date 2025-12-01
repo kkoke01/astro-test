cd82dbfa83850b5151ef01ac8f7b912ac82b0de1
马馨怡 <maxinyi@maxinyideMacBook-Pro.local>
2025-12-01 22:10:41 +0900

    delete nase

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://kkoke01.github.io',
-    base: 'astro-test',
+    // base: 'astro-test',
 });
